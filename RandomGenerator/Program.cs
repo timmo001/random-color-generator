d6beec4ffca78d612033c6aa19681f73da1a034d
Aidan <28114703+timmo001@users.noreply.github.com>
2018-06-18 16:30:43 +0100

    Complete with light colors only

--- RandomGenerator/Program.cs
+++ RandomGenerator/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
 
-namespace RandomGenerator
+namespace RandomColorGenerator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (StreamWriter file = new StreamWriter(args[1], true))
+            {
+                var random = new Random();
+                for (int i = 0; i < Int32.Parse(args[0]); i++)
+                {
+                    file.WriteLine(String.Format("{0}", ColorTranslator.ToHtml(
+                        Color.FromArgb(random.Next(150, 255), random.Next(150, 255), random.Next(150, 255))
+                        )));
+                }
+            }
         }
     }
 }
